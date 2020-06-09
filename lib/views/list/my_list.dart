9771fae1fcf10cbb412f8dcba8b9d23c41627967
Yennifer Herrera <yenniferh@uninorte.edu.co>
2020-06-09 15:39:25 -0500

    MyListTile

--- lib/views/list/my_list.dart
+++ lib/views/list/my_list.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_for_friends/views/components/my_list_tile.dart';
 
 class MyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
         body: Container(
-          child: Column(
-            children: <Widget>[
-              Text("Banana - 200 und"),
-              Text("Cebolla - 300 und"),
-              Text("Tomate - 300 und"),
-            ],
-          ),
-        ));
+      child: Column(
+        children: <Widget>[
+          MyListTile(name: "Banana", price: "200 und"),
+          MyListTile(name: "Huevo", price: "350 und"),
+          MyListTile(name: "Cebolla", price: "300 und"),
+        ],
+      ),
+    ));
   }
 }
