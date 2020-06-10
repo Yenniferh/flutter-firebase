f2cdb14086bc48b4797f71841aa949f8faadaf13
Yennifer Herrera <yenniferh@uninorte.edu.co>
2020-06-09 21:19:50 -0500

    Shopping list

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_for_friends/services/firebase.dart';
-
-import 'package:shopping_for_friends/views/components/app-drawer.dart';
 import 'package:shopping_for_friends/views/Product/productList.dart';
 import 'package:shopping_for_friends/views/friends/friends_list.dart';
-
+import 'package:shopping_for_friends/views/shopping/shopping_list.dart';
 import '../../locator.dart';
 import '../Product/productList.dart';
 import '../list/my_list.dart';
@@ -31,9 +29,10 @@ class _HomeState extends State<Home> {
   final AuthService _auth = locator<AuthService>();
   int _currentIndex = 0;
   final List<Widget> _children = [
-    MyList(),
     ProductList(),
-    FriendsShoppingList()
+    MyList(),
+    FriendsShoppingList(),
+    ShoppingList()
   ];
   @override
   Widget build(BuildContext context) {
@@ -54,23 +53,27 @@ class _HomeState extends State<Home> {
             ),
           ],
         ),
-        /*  drawer: AppDrawer()
-          ,  */
-          body:_children[_currentIndex],
+        body: _children[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           onTap: onTabTapped, // new
           currentIndex: _currentIndex,
+          unselectedItemColor: Colors.grey[600],
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.lightBlueAccent[400],
           items: [
             BottomNavigationBarItem(
-              icon: new Icon(Icons.list),
-              title: new Text('My list'),
+              icon: Icon(Icons.shopping_basket),
+              title: Text('Product List'),
             ),
             BottomNavigationBarItem(
-              icon: new Icon(Icons.shopping_basket),
-              title: new Text('Product List'),
+              icon: Icon(Icons.person),
+              title: Text('My list'),
             ),
             BottomNavigationBarItem(
-                icon: Icon(Icons.person), title: Text("My friend's lists"))
+                icon: Icon(Icons.group_add),
+                title: Text("Friend's Shopping List")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list), title: Text("Shopping List"))
           ],
         ),
       ),
---
+++ lib/views/shopping/shopping_list.dart
@@ -0,0 +1,412 @@
+import 'package:flutter/material.dart';
+
+class ShoppingList extends StatelessWidget {
+  const ShoppingList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: Text("My Shopping List",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54)),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "My items",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Text("My Friend's Shopping List",
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54)),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "Carlos",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "Julia",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "Robert",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "Javier",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10))),
+                    child: Text(
+                      "Mario",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(0),
+                    color: Colors.white,
+                    child: SizedBox(
+                      height: 150,
+                      child: ListView(
+                        children: <Widget>[
+                          ListTile(
+                            title: Text("Banana"),
+                            subtitle: Text("200/und * 12"),
+                          ),
+                          ListTile(
+                            title: Text("Huevo"),
+                            subtitle: Text("350/und * 30"),
+                          ),
+                          ListTile(
+                            title: Text("Atún"),
+                            subtitle: Text("3800/und * 3"),
+                          ),
+                          ListTile(
+                            title: Text("Cebolla"),
+                            subtitle: Text("300/und * 12"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10))),
+                    child: Text(
+                      "Total: \$25.000",
+                      style: TextStyle(color: Colors.white, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Total: \$150.000",
+                style: TextStyle(color: Colors.redAccent[400], fontSize: 24),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ],
+        )
+      ],
+    ));
+  }
+}
